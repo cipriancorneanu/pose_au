55e12b8a6eb57e542436c9e75745e435e70e9828
CiprianCorneanu <CiprianCorneanu>
2018-04-26 19:45:11 +0200

    Draft of triplet data sampling from Fera2017. Not functional.

--- dataset.py
+++ dataset.py
@@ -103,58 +103,75 @@ class Fera2017DatasetTriplet(Dataset):
         self.partition = partition
         self.n_poses = 9
         self.partition_key = 'TR' if partition == 'train' else 'VA'
-
-        f = h5py.File(self.root_dir+'fera_train.h5',
-                      'r') if self.partition == 'train' else h5py.File(self.root_dir+'fera_test.h5', 'r')
-        self.subjects = tsubs if tsubs else f[self.partition_key].keys()
+        self.hf = h5py.File(self.root_dir+'fera_train.h5',
+                            'r') if self.partition == 'train' else h5py.File(self.root_dir+'fera_test.h5', 'r')
+        self.subjects = tsubs if tsubs else self.hf[self.partition_key].keys()
         self.poses = tposes if tposes else range(self.n_poses)
-        self.idxmap = self.get_idxmap_from_h5py()
+        self.dataset_idx = self.get_dataset_idx()
+        self.idx_pn = self.get_idx_pn()
         self.transform = transform
 
-    def get_idxmap_from_h5py(self):
-        idx_map = {}
+    def get_dataset_idx(self):
+        map = {}
         offset = 0
 
-        path = self.root_dir + \
-            'fera_train.h5' if self.partition == 'train' else self.root_dir+'fera_test.h5'
+        for sub in self.subjects:
+            for task in self.hf[self.partition_key+'/'+sub].keys():
+                for pose in self.poses:
+                    key = self.partition_key+'/'+sub+'/'+task+'/'+str(pose)
 
-        with h5py.File(path, 'r') as hf:
-            for sub in self.subjects:
-                for task in hf[self.partition_key+'/'+sub].keys():
-                    for pose in self.poses:
-                        key = self.partition_key+'/'+sub+'/'+task+'/'+str(pose)
+                    if key in self.hf:
+                        n = self.hf[key]['faces'].shape[0]
+                        map[key] = {'idx': np.arange(offset, offset+n),
+                                    'aus': np.array(self.hf[key]['aus'])}
 
-                        if key in hf:
-                            n = hf[key]['faces'].shape[0]
-                            idx_map[key] = np.arange(offset, offset+n)
+                        if self.verbose:
+                            print('{}: ({}, {}), {}'.format(
+                                sub+'/'+task+'/'+str(pose), offset, offset+n, n))
 
-                            if self.verbose:
-                                print('{}: ({}, {}), {}'.format(
-                                    sub+'/'+task+'/'+str(pose), offset, offset+n, n))
+                        offset = offset + n
 
-                            offset = offset + n
-        return idx_map
+        return map
+
+    def get_idx_pn(self):
+        ''' For every sample in the dataset find a positive and a negative (a, p, n)'''
+
+        ''' Create triplet'''
+        '''
+        the requested idx is the anchor
+        2. Get active classes from anchor
+        3. Randmly picl one of the active classes
+        4. Find POSITIVE: Find sample from same subject but DIFFERENT pose same active class.
+        4.1 What if I don't find?
+        5. Fing NEGATIVE: Any sample where this class is not active. 
+        '''
+        return [(x, x) for x in np.arange(len(self))]
 
     def __len__(self):
-        return np.sum([v.size for v in self.idxmap.itervalues()])
+        return np.sum([v['idx'].size for v in self.dataset_idx.itervalues()])
+
+    def f(self, idx):
+        for k, v in self.dataset_idx.iteritems():
+            if idx >= v['idx'].min() and idx <= v['idx'].max():
+                key, i = k, idx-v['idx'].min()
+
+        x = self.hf[key]['faces'][i]
+        y = self.hf[key]['aus'][i]
+
+        return x, y
 
     def __getitem__(self, idx):
-        ''' Write triplet batch sampling code here'''
-        for k, v in self.idxmap.iteritems():
-            if idx >= v.min() and idx <= v.max():
-                key, i = k, idx-v.min()
+        idx_p, idx_n = self.idx_pn[idx]
 
-        path = self.root_dir + \
-            'fera_train.h5' if self.partition == 'train' else self.root_dir+'fera_test.h5'
-
-        with h5py.File(path, 'r') as hf:
-            x = hf[key]['faces'][i]
-            y = hf[key]['aus'][i]
+        tr = [self.f(x) for x in [idx, idx_p, idx_n]]
 
         if self.transform:
-            (x, y) = self.transform((x, y))
+            t_tr = [self.transform((x, y)) for x, y in tr]
 
-        return (x, y, key)
+        return {'a': {'x': t_tr[0][0], 'y': t_tr[0][1]},
+                'p': {'x': t_tr[1][0], 'y': t_tr[1][1]},
+                'n': {'x': t_tr[2][0], 'y': t_tr[2][1]}
+                }
 
 
 class ToTensor(object):
@@ -177,15 +194,20 @@ class ToTensor(object):
 if __name__ == '__main__':
     tsfm = ToTensor()
     dt_tr = Fera2017DatasetTriplet('/data/data1/datasets/fera2017/',
-                                   partition='train', tsubs=None, tposes=[1, 6, 7], transform=tsfm, verbose=True)
+                                   partition='train', tsubs=None, tposes=None, transform=tsfm, verbose=False)
+
+    for i in range(0, len(dt_tr), 100):
+        t = dt_tr[i]
+        print('i:{},  a_y:{}, p_y:{}, n_y:{}'.format(
+            i, t['a']['y'].numpy(), t['p']['y'].numpy(), t['n']['y'].numpy()))
+
     '''
     print('----------------')
     dt_val = Fera2017Dataset('/data/data1/datasets/fera2017/',
                              partition='validation', tsubs=None, tposes=[1, 6, 7], transform=tsfm, verbose=True)
     '''
-
-    dl_tr = DataLoader(dt_tr, batch_size=64, shuffle=True, num_workers=4)
     '''
+    dl_tr = DataLoader(dt_tr, batch_size=64, shuffle=True, num_workers=4)
     dl_val = DataLoader(dt_val, batch_size=64, shuffle=True, num_workers=4)
     '''
     '''
