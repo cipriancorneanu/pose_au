9669898a8889635c2217dc86ffd5d3164aba7165
CiprianCorneanu <CiprianCorneanu>
2018-06-21 15:07:37 +0200

    Added triplet sampler

--- dataset.py
+++ dataset.py
@@ -2,9 +2,82 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import Sampler
 from facepp.reader.reader import read_csv
 import h5py
 import random
+import time
+import cPickle as pkl
+from operator import itemgetter
+
+
+class TripletSampler(Sampler):
+    def __init__(self, data_source, batch_size=32, npair=False, min_nitems=2, nclasses=None, remove_neutral=True):
+        self.data_source = data_source
+        self.labels = [[x[0], x[1]] for x in data_source.get_labels_idx()]
+        self.batch_size = batch_size
+        self.npair = npair
+        self.min_nitems = min_nitems
+        self.n_classes = nclasses
+        self.idxs = self.generate_indxs(
+            batch_size, remove_neutral, nclasses, min_nitems, npair)
+
+    def generate_indxs(self, batch_size=32, remove_neutral=True, nclasses=None, min_nitems=2, npair=False):
+        ''' Remove  neutral '''
+        if remove_neutral:
+            labels = [x for x in self.labels if x[0] != '0000000000']
+
+        ''' Pick first nclasses '''
+        if nclasses:
+            labels = labels[:nclasses]
+
+        idxs, idx_pool = [], []
+
+        print([[x[0], len(x[1])] for x in labels])
+
+        while len(labels) > self.batch_size/2:
+            ''' Remove labels with fewer than 2 samples '''
+            labels = [[x[0], x[1]]
+                      for x in labels if len(x[1]) >= min_nitems]
+
+            '''print([[x[0], len(x[1])] for x in labels])'''
+
+            '''
+            print('No. of remaining samples: {}'.format(
+                np.sum([len(x[1]) for x in labels])))
+            '''
+            ''' Pick labels following label occurence distribution '''
+            label_choice = np.random.choice(
+                np.concatenate([[i]*len(x[1]) for i, x in enumerate(labels)]), size=batch_size/2)
+
+            for l in label_choice:
+                ''' Randomly pick 2 indices '''
+                ind = np.random.choice(labels[l][1], 2)
+
+                ''' Update indices list '''
+                labels[l][1] = list(set(labels[l][1]) - set(ind))
+
+                idx_pool.append(ind)
+
+            ''' From the sampled pool create mini_batches of indices '''
+            if npair:
+                idxs.append(self._generate_npair(np.concatenate(idx_pool)))
+            else:
+                idxs.append(np.concatenate(idx_pool))
+            idx_pool = []
+
+        return np.concatenate(idxs)
+
+    def __iter__(self):
+        return iter(self.idxs)
+
+    def _generate_npair(self, pool):
+        heads, tails = ([[x, x+1] for x in range(len(pool))[::2]],
+                        [range(0, x)+range(x+2, len(pool)) for x in range(len(pool))[::2]])
+        return np.concatenate([np.hstack((pool[h], pool[t])) for h, t in zip(heads, tails)])
+
+    def __len__(self):
+        return len(self.idxs)
 
 
 class Fera2017Dataset(Dataset):
@@ -22,36 +95,8 @@ class Fera2017Dataset(Dataset):
         self.idxmap = self.get_idxmap_from_h5py()
         self.transform = transform
 
-    def get_idxmap_from_csv(self):
-        ''' Map 'subject/task/pose' to index _list'''
-        idx_map = {}
-        offset = 0
-        for sub in self.subjects:
-            '''Get all files from sub'''
-            files = sorted([f for f in os.listdir(
-                self.root_dir+self.partition+'/occ') if sub in f])
-
-            for f in files:
-                (dataset, partition, subject, task) = f.split(
-                    '.')[0].split('_')
-
-                n = np.asarray(read_csv(self.root_dir+self.partition+'/occ/'+f)
-                               )[1:, 1:].shape[0]
-
-                for pose in self.poses:
-                    ''' Add entry to dictionary and update offset'''
-                    idx_map[self.partition_key + '/' + sub + '/' + task + '/' +
-                            str(pose)] = np.arange(offset, offset+n)
-                    if self.verbose:
-                        print('{}: ({}, {}), {}'.format(
-                            sub+'/'+task+'/'+str(pose), offset, offset+n, n))
-
-                    offset = offset + n
-
-        return idx_map
-
     def get_idxmap_from_h5py(self):
-        idx_map = {}
+        idx_map = []
         offset = 0
 
         path = self.root_dir + \
@@ -65,209 +110,59 @@ class Fera2017Dataset(Dataset):
 
                         if key in hf:
                             n = hf[key]['faces'].shape[0]
-                            idx_map[key] = np.arange(offset, offset+n)
 
-                            if self.verbose:
-                                print('{}: ({}, {}), {}'.format(
-                                    sub+'/'+task+'/'+str(pose), offset, offset+n, n))
+                            for i in range(n):
+                                item = {'idx': offset+i, 'ridx': i,
+                                        'label': ''.join(str(e) for e in hf[key]['aus'][i]),
+                                        'key': key}
+                                idx_map.append(item)
+
+                                if self.verbose:
+                                    print('{}:{}'.format(offset+i, item))
 
                             offset = offset + n
         return idx_map
 
+    def get_label(self, label):
+        '''Return items for a specific label '''
+        return [item for item in self.idxmap if item['label'] == label]
+
+    def get_label_field(self, label, field):
+        ''' Return indices for a specific label'''
+        return [item[field] for item in self.idxmap if item['label'] == label]
+
+    def get_labels_occ(self):
+        ''' Return a list of all individual labels sorted by number of occurences in the dataset '''
+        labels = list(set([item['label'] for item in self.idxmap]))
+        return sorted([(l, len(self.get_label(l)))
+                       for l in labels], key=itemgetter(1), reverse=True)
+
+    def get_labels_idx(self):
+        '''
+        Return a list of all individual labels and their indices
+        sorted by number of occurences in the dataset
+        '''
+        labels_occ = self.get_labels_occ()
+        return [(x[0], self.get_label_field(x[0], 'idx')) for x in labels_occ]
+
     def __len__(self):
-        return np.sum([v.size for v in self.idxmap.itervalues()])
+        return np.sum(len(self.idxmap))
 
     def __getitem__(self, idx):
         ''' Write batch sampling code here'''
-        for k, v in self.idxmap.iteritems():
-            if idx >= v.min() and idx <= v.max():
-                key, i = k, idx-v.min()
+        item = self.idxmap[idx]
 
         path = self.root_dir + \
             'fera_train.h5' if self.partition == 'train' else self.root_dir+'fera_test.h5'
 
         with h5py.File(path, 'r') as hf:
-            x = hf[key]['faces'][i]
-            y = hf[key]['aus'][i]
+            x = hf[item['key']]['faces'][item['ridx']]
+            y = hf[item['key']]['aus'][item['ridx']]
 
         if self.transform:
             (x, y) = self.transform((x, y))
 
-        return (x, y, key)
-
-
-class Fera2017DatasetTriplet(Dataset):
-    def __init__(self, root_dir, partition='train', tsubs=None, tposes=None, transform=None, verbose=False):
-        self.verbose = verbose
-        self.root_dir = root_dir + '/'
-        self.partition = partition
-        self.n_poses = 9
-        self.partition_key = 'TR' if partition == 'train' else 'VA'
-        self.hf = h5py.File(self.root_dir+'fera_train.h5',
-                            'r') if self.partition == 'train' else h5py.File(self.root_dir+'fera_test.h5', 'r')
-        self.subjects = tsubs if tsubs else self.hf[self.partition_key].keys()
-        self.poses = tposes if tposes else range(self.n_poses)
-        self.dataset_idx = self.get_dataset_idx()
-        self.idx_pn = self.get_idx_pn(verbose)
-        self.transform = transform
-
-    def get_dataset_idx(self):
-        map = {}
-        offset = 0
-
-        for sub in self.subjects:
-            for task in self.hf[self.partition_key+'/'+sub].keys():
-                for pose in self.poses:
-                    key = self.partition_key+'/'+sub+'/'+task+'/'+str(pose)
-
-                    if key in self.hf:
-                        aus = np.array(self.hf[key]['aus'])
-                        aus = aus[np.where(aus.any(axis=1))[0]]
-                        n = aus.shape[0]
-
-                        print(np.array(self.hf[key]['aus'].shape), n)
-
-                        '''
-                        PROBLEM HERE with the indexing
-                        '''
-
-                        map[key] = {'idx': np.arange(offset, offset+n),
-                                    'aus': aus}
-
-                        if self.verbose:
-                            print('{}: ({}, {}), {}'.format(
-                                sub+'/'+task+'/'+str(pose), offset, offset+n, n))
-
-                        offset = offset + n
-
-        return map
-
-    def get_idx_pn(self, verbose=False):
-        ''' For every sample in the dataset find a positive and a negative (a, p, n)'''
-
-        map = []
-        for x_a in np.arange(len(self)):
-            if x_a % 100 == 0:
-                print(x_a)
-
-            _, y_a, _, _ = self.get(x_a)
-
-            ''' Get active classes from anchor '''
-            active_classes = np.where(y_a == 1)[0]
-
-            ''' Randomly pick pivot class from one of the active classes '''
-            if len(active_classes) > 0:
-                pivot_class = active_classes[np.random.randint(
-                    0, len(active_classes))]
-
-                ''' Get positive '''
-                x_p = self.get_positive(x_a, pivot_class, verbose=False)
-
-                ''' Get negative '''
-                x_n = self.get_negative(x_a, pivot_class, verbose=False)
-
-            else:
-                print('No active class. Return anchor as postive and negative')
-                x_p, x_n = x_a, x_a
-
-            map.append((x_p, x_n))
-
-        return map
-
-    def get_positive(self, idx_a, pivot, verbose=False):
-        _, y_a, key_a, ridx_a = self.get(idx_a)
-        if verbose:
-            print('---------------------')
-            print('key: {}, idx_a: {}, ridx_a:{}, pivot: {}'.format(
-                key_a, idx_a, ridx_a, pivot))
-
-        ''' Get different pose '''
-        root_a, pose_a = key_a[:-1],  int(key_a.split('/')[3])
-
-        if verbose:
-            print('pose anchor: {}'.format(pose_a))
-
-        pose_p = random.choice(list(set(self.poses) - set([pose_a])))
-        key_p = root_a + str(pose_p)
-
-        if verbose:
-            print('key positive :{}'.format(key_p))
-
-        ''' Get same samples from same subject but different pose '''
-        ridx_p = ridx_a
-        idx_p = self.dataset_idx[key_p]['idx'][0] + ridx_p
-
-        if verbose:
-            print('key_p: {}, idx_p: {}'.format(key_p, idx_p))
-
-        return idx_p
-
-    def get_negative(self, idx_a, pivot, verbose=False):
-        '''
-        TODO: deal with the case when no negative is located in same sequence!
-        '''
-        _, y_a, key_a, ridx_a = self.get(idx_a)
-        if verbose:
-            print('---------------------')
-            print('key: {}, idx_a: {}, ridx_a:{}, pivot: {}'.format(
-                key_a, idx_a, ridx_a, pivot))
-
-        ''' Get all samples from same subject and same pose'''
-        pool = np.array(self.hf[key_a]['aus'])
-
-        if verbose:
-            print('All samples: {}'.format(pool.shape))
-
-        ''' Filter from these samples all that have pivot non-active '''
-        if(pool.size > 0):
-            ridxs_n = np.where(pool[:, pivot] == 0)[0]
-
-            if ridxs_n.size > 0:
-                if verbose:
-                    print('Pool of potential negatives: {}'.format(
-                        pool[ridxs_n].shape))
-
-                ''' Randomply pick one '''
-                ridx_n = np.random.choice(np.squeeze(ridxs_n))
-
-                if verbose:
-                    print('ridx_n: {}'.format(ridx_n))
-
-                idx_n = self.dataset_idx[key_a]['idx'][0] + ridx_n
-
-                if verbose:
-                    print('key_n: {}, idx_n: {}'.format(key_a, idx_n))
-            else:
-                print('No negatives found. Return anchor.')
-                idx_n = idx_a
-
-        return idx_n
-
-    def __len__(self):
-        return np.sum([v['idx'].size for v in self.dataset_idx.itervalues()])
-
-    def get(self, idx):
-        for k, v in self.dataset_idx.iteritems():
-            if idx >= v['idx'].min() and idx <= v['idx'].max():
-                key, i = k, idx-v['idx'].min()
-
-        x = self.hf[key]['faces'][i]
-        y = self.hf[key]['aus'][i]
-
-        return x, y, key, i
-
-    def __getitem__(self, idx):
-        idx_p, idx_n = self.idx_pn[idx]
-
-        tr = [self.get(x) for x in [idx, idx_p, idx_n]]
-
-        if self.transform:
-            t_tr = [self.transform((x, y)) for x, y, _, _ in tr]
-
-        return {'a': {'x': t_tr[0][0], 'y': t_tr[0][1]},
-                'p': {'x': t_tr[1][0], 'y': t_tr[1][1]},
-                'n': {'x': t_tr[2][0], 'y': t_tr[2][1]}
-                }
+        return (x, y, item['key'])
 
 
 class ToTensor(object):
@@ -287,30 +182,91 @@ class ToTensor(object):
         return (torch.from_numpy(im),  torch.from_numpy(aus))
 
 
+def hamming_distance(s1, s2):
+    """Return the Hamming distance between equal-length sequences"""
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(el1 != el2 for el1, el2 in zip(s1, s2))
+
+
+def explore_labels():
+    print('Total number of samples : {}'.format(len(dt_tr)))
+
+    ''' Get complete list of labels  '''
+    labels = dt_tr.get_all_labels()
+    labels_occ = sorted([(l, len(dt_tr.get_label(l)))
+                         for l in labels], key=itemgetter(1), reverse=True)
+    print('The complete list of labels is : {}'.format(labels_occ))
+    print('There are {} individual labels'.format(len(labels_occ)))
+
+    labels_occ_100 = [x[1] for x in labels_occ if x[1] >= 100]
+    print('labels_100: n_labels {}: n_samples {}'.format(
+        len(labels_occ_100), np.sum(labels_occ_100)))
+
+    ''' Get ordered list of labels according to hamming distance to anchor'''
+    anchor = '0000000000'
+    h_list = [(l, hamming_distance(anchor, l))
+              for l in labels]
+    print('List h_distance from anchor {} : {}'.format(anchor, h_list))
+
+    ''' Get all samples for label '''
+    for l in labels_occ[:10]:
+        pool = dt_tr.get_label(l[0])
+
+        '''Sample at most n_samples'''
+        n_samples = 64
+        pool = [pool[x] for x in np.random.choice(np.arange(len(pool)),
+                                                  min(len(pool), n_samples))]
+        to_save = [dt_tr[x['idx']] for x in pool]
+
+        f = open(l[0] + '_' + str(l[1]) + '.pkl', 'wb')
+        pkl.dump({'dt': to_save}, f, protocol=pkl.HIGHEST_PROTOCOL)
+
+    ''' Get all samples  at H distance from anchor '''
+    for h in range(10):
+        labels_h = [x[0] for x in h_list if x[1] == h]
+        print('{} labels at distance {} from anchor : {}'.format(
+            len(labels_h), h, labels_h))
+
+        pool = np.concatenate([dt_tr.get_label(l) for l in labels_h])
+
+        print(len(pool))
+
+        '''Sample at most n_samples'''
+        n_samples = 64
+        pool = [pool[x] for x in np.random.choice(np.arange(len(pool)),
+                                                  min(len(pool), n_samples))]
+        to_save = [dt_tr[x['idx']] for x in pool]
+
+        f = open(anchor + '_' + str(h)+'.pkl', 'wb')
+        pkl.dump({'dt': to_save}, f, protocol=pkl.HIGHEST_PROTOCOL)
+
+
 if __name__ == '__main__':
     tsfm = ToTensor()
-    '''
-    dt_tr = Fera2017DatasetTriplet('/data/data1/datasets/fera2017/',
-                                   partition='train', tsubs=['F001', 'F002'],
-                                   tposes=[1, 6, 7], transform=tsfm, verbose=True)
-
-    for i in range(0, len(dt_tr), 100):
-        t = dt_tr[i]
-        print('i:{},  a_y:{}, p_y:{}, n_y:{}'.format(
-            i, t['a']['y'].numpy(), t['p']['y'].numpy(), t['n']['y'].numpy()))
-    '''
+    start_time = time.time()
+    b_sz = 32
     dt_tr = Fera2017Dataset('/data/data1/datasets/fera2017/',
-                            partition='train', tsubs=None, tposes=None, transform=tsfm, verbose=True)
+                            partition='train', tposes=[6],
+                            verbose=False)
+    dt_te = Fera2017Dataset('/data/data1/datasets/fera2017/',
+                            partition='validation', tposes=[6],
+                            verbose=False)
 
-    print('----------------')
-    dt_val = Fera2017Dataset('/data/data1/datasets/fera2017/',
-                             partition='validation', tsubs=None, tposes=None, transform=tsfm, verbose=True)
+    sampler_tr = TripletSampler(
+        dt_tr, batch_size=b_sz, nclasses=20, npair=True)
+    sampler_te = TripletSampler(
+        dt_te, batch_size=b_sz, nclasses=20, npair=False)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    dl_tr = DataLoader(dt_tr, sampler=sampler_tr, batch_size=b_sz)
+    dl_te = DataLoader(dt_te, sampler=sampler_te, batch_size=b_sz)
+
+    print('len dl_tr :{} '.format(len(dl_tr)))
+    print('len dl_te :{} '.format(len(dl_te)))
+
     '''
-    dl_tr = DataLoader(dt_tr, batch_size=64, shuffle=True, num_workers=4)
-    dl_val = DataLoader(dt_val, batch_size=64, shuffle=True, num_workers=4)
-    '''
-    '''
-    for i, (x, y, seq) in enumerate(dl_tr):
-        print('{}: {} {}, mean = {}'.format(
-            i, x.shape, y.shape, torch.mean(x)))
+    for iter, (x, y, _) in enumerate(dl_tr):
+        print('iter: {}, {}'.format(iter, y))
     '''
