f55910f2aac67d03a0673600a44d618b077651a3
CiprianCorneanu <CiprianCorneanu>
2018-06-21 15:11:00 +0200

    Minor updates

--- gdvm.py
+++ gdvm.py
@@ -22,7 +22,7 @@ from topologies import GDVM, GDVM_ResNet
 import torchvision.models as models
 from logger import Logger
 from utils import save_checkpoint
-from losses import FocalLoss, KLD, BCE
+from losses import bce_loss, kld
 
 parser = argparse.ArgumentParser(description='VAE.')
 parser.add_argument("--path", default='/data/data1/datasets/fera2017/',
@@ -33,8 +33,6 @@ parser.add_argument("--epochs", type=int, default=10,
                     help='number of training epochs(default=10)')
 parser.add_argument("--encoder", default='resnet18',
                     help='Topology used to represent the input')
-parser.add_argument("--criterion", default='bce',
-                    help='Chooses the discriminative criterion in the loss: bce = binary cross entropy, focal: focal loss')
 parser.add_argument("--lr", type=float, default=0.001,
                     help='optimization learning rate')
 parser.add_argument("--size_latent", type=int, default=256,
@@ -43,17 +41,34 @@ parser.add_argument("--dropout", type=float, default=0.2,
                     help='Dropout probability.')
 parser.add_argument("--log_interval", type=int, default=100,
                     help='how many iterations to wait before logging info')
-parser.add_argument("--beta", type=float, default=1,
+parser.add_argument("--beta", type=float, default=0,
                     help='how much the KL weights in the final loss')
-parser.add_argument("--gamma", type=float, default=0,
-                    help='parameter of the focal loss, if 0 the focal loss is equivalent to the binary cross entropy')
-parser.add_argument("--alpha", type=float, default=1,
-                    help='parameter of the focal loss')
+parser.add_argument("--aus",  default='[0,1,2,3,4,5,6,7,8,9]',
+                    help='Select specific classes.')
+parser.add_argument("--poses",  default='[0,1,2,3,4,5,6,7,8,9]',
+                    help='Select specific poses.')
 parser.add_argument("--quick_test", type=bool, default=False,
                     help='For quick testing use just one subject and pose.')
 args = parser.parse_args()
 
-n_classes = 10
+if args.aus == 'all':
+    aus = range(10)
+else:
+    aus = map(int, args.aus.strip('[]').split(','))
+
+if args.poses == 'all':
+    poses = range(1, 10)
+else:
+    poses = map(int, args.poses.strip('[]').split(','))
+
+t_subs_tr, t_subs_te = (None, None)
+
+if args.quick_test:
+    aus = range(10)
+    poses = [6]
+    t_subs_tr, t_subs_te = (['F001', 'F002', 'F003'], ['F007', 'F008', 'F009'])
+
+n_classes = len(aus)
 
 if args.encoder == 'resnet18':
     encoder = models.resnet18()
@@ -71,35 +86,28 @@ print('Total number of params in model: {}'.format(n_params))
 model.cuda()
 
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
-poses = [1, 6, 7]
-eval_thresholds = np.arange(0.05, 1, 0.1)
+eval_thresholds = np.arange(0.05, 1, 0.05)
 tsfm = ToTensor()
 
-oname = 'gdvm_fl' + args.encoder + '_beta_' + \
-    str(args.beta) + '_gamma_' + \
-    str(args.gamma) + '_latent_' + \
+oname = 'gdvm_aus_' + str(aus) + '_poses_' + str(poses) + '_encoder_' + args.encoder + '_beta_' + \
+    str(args.beta) + '_latent_' + \
     str(args.size_latent) + '_lr_' + str(args.lr)
 
 print(oname)
 
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer, factor=0.2, patience=3, verbose=True, threshold=0.05)
-logger = Logger('./logs/'+oname+'/')
-
-''' If just quick test reduce the training/test subjcets and poses to minimum '''
-(t_subs_tr, t_subs_te) = (['F001', 'F002', 'F003'], ['F007', 'F008', 'F009']
-                          ) if args.quick_test else (None, None)
-poses = ([6]) if args.quick_test else poses
+    optimizer, factor=0.1, patience=10, verbose=True, threshold=0.01)
+logger = Logger('./logs/post_final/'+oname+'/')
 
 dt_train = Fera2017Dataset('/data/data1/datasets/fera2017/',
-                           partition='train', tsubs=t_subs_tr, tposes=poses, transform=tsfm)
+                           partition='train', tsubs=t_subs_tr, tposes=poses, transform=tsfm, verbose=False)
 dl_train = DataLoader(dt_train, batch_size=args.batch_size,
                       shuffle=True, num_workers=4)
 
 dl_test, n_iter_test = [], []
 for pose in poses:
     dt_test = Fera2017Dataset('/data/data1/datasets/fera2017/',
-                              partition='validation', tsubs=t_subs_te,  tposes=[pose], transform=tsfm)
+                              partition='validation', tsubs=t_subs_te,  tposes=[pose], transform=tsfm, verbose=False)
     n_iter_test.append(len(dt_test)/args.batch_size)
     dl_test.append(DataLoader(dt_test, batch_size=args.batch_size,
                               shuffle=True, num_workers=4))
@@ -111,43 +119,34 @@ print('n_iter in test per pose: {}, total: {}'.format(
     n_iter_test, n_iter_test_total))
 
 
-FL = FocalLoss(num_classes=n_classes,
-               alpha=args.alpha, gamma=args.gamma)
-
-''' Define criterion and regularization in loss '''
-regularization = KLD
-if args.criterion == 'bce':
-    criterion = BCE
-elif args.criterion == 'focal':
-    criterion = FL
-
-regularization = KLD
-
-
-def train(criterion, regularization):
+def train():
+    print(model.training)
     model.train()
+    print(model.training)
+
     acc_loss, acc_crit, acc_reg = [], [], []
 
     for iter, (data, target, _) in enumerate(dl_train):
+        target = torch.clamp(target[:, aus], 0, 1)
         data, target = data.cuda(), target.cuda()
         data, target = Variable(data).float(), Variable(target).float()
 
         optimizer.zero_grad()
         pred, mu, logvar = model(data)
-        crit_val = criterion(pred, target) / len(data)
-        reg_val = regularization(mu, logvar) / len(data)
+        crit_val = bce_loss(pred, target)
+        reg_val = kld(mu, logvar) / len(data)
 
-        loss = crit_val + args.beta*reg_val
+        loss = crit_val + beta*reg_val
         loss.backward()
         optimizer.step()
 
         if iter % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{}]\tLoss: {:.4f} + {}*{:.4f} = {:.4f}'.format(
-                epoch, iter, n_iter_train, crit_val.data, args.beta, reg_val.data, loss.data))
+            print('Train Epoch: {} [{}/{}]\tLoss: {:.4f} + {}*{:.8f} = {:.4f}'.format(
+                epoch, iter, n_iter_train, crit_val.data, beta, reg_val.data, loss.data))
 
             info = {
                 'loss_train': loss.data,
-                'criterion_train': crit_val.data,
+                'crit_train': crit_val.data,
                 'reg_train': reg_val.data
             }
 
@@ -162,7 +161,7 @@ def train(criterion, regularization):
     return np.mean(acc_loss), np.mean(acc_crit), np.mean(acc_reg)
 
 
-def test(criterion, regularization, n_runs):
+def test():
     model.eval()
     f1s, acc_loss, acc_crit, acc_reg = [], [], [], []
     for i, dl_test_pose in enumerate(dl_test):
@@ -170,26 +169,22 @@ def test(criterion, regularization, n_runs):
         print(
             '-----------------------------------Evaluating POSE {} ------------------------- '.format(poses[i]))
         for iter, (data, target, _) in enumerate(dl_test_pose):
+            target = torch.clamp(target[:, aus], 0, 1)
             data, target = data.cuda(), target.cuda()
             data, target = Variable(data).float(
             ), Variable(target).float()
 
             with torch.no_grad():
-                ''' Pass n_runs times through model '''
-                outputs = [model(data) for run in range(n_runs)]
+                pred, mu, logvar = model(data)
 
-                ''' Compute loss every pass '''
-                for (pred, mu, logvar) in outputs:
-                    crit_val = criterion(pred, target) / len(data)
-                    reg_val = regularization(mu, logvar) / len(data)
-                    loss = crit_val + args.beta * reg_val
-                    acc_crit.append(crit_val.data)
-                    acc_reg.append(reg_val.data)
-                    acc_loss.append(loss.data)
+                pred = F.sigmoid(pred)
+                crit_val = bce_loss(pred, target)
+                reg_val = kld(mu, logvar) / len(data)
+                loss = crit_val + beta*reg_val
 
-                ''' Final prediction is the mean of all passes'''
-                pred = np.mean([F.sigmoid(p.data).cpu().numpy()
-                                for (p, mu, lvar) in outputs], axis=0)
+                acc_crit.append(crit_val.data)
+                acc_reg.append(reg_val.data)
+                acc_loss.append(loss.data)
 
                 preds.append(pred)
                 targets.append(target.data.cpu().numpy())
@@ -222,11 +217,9 @@ def test(criterion, regularization, n_runs):
         f1s.append(f1_pose)
 
     ''' Log validation loss '''
-    loss, criterion, regularization = np.mean(
-        acc_loss), np.mean(acc_crit), np.mean(acc_reg)
-    info = {'loss_test': loss,
-            'crit_test': criterion,
-            'reg_test': regularization}
+    info = {'loss_test': np.mean(acc_loss),
+            'crit_test': np.mean(acc_crit),
+            'reg_test': np.mean(acc_reg)}
 
     for tag, value in info.items():
         logger.scalar_summary(tag, value, epoch)
@@ -239,30 +232,51 @@ def test(criterion, regularization, n_runs):
         for tag, value in info.items():
             logger.scalar_summary(tag, value, epoch)
 
-    return loss, criterion, regularization,  f1s
+    return np.mean(acc_loss), np.mean(acc_crit), np.mean(acc_reg),  f1s
 
 
+def update_params(param, crit, patience=3, update=0.5):
+    var = 0.1
+    if epoch > patience and np.mean(crit[-patience:])/crit[-1] < (1+var):
+        param = (1+update)*param
+        print('We are on a plateau. Updating parameter to: {}'.format(param))
+    else:
+        print('No update.')
+
+    return param
+
+
+beta = args.beta
+crit_train_list = []
+best_f1 = 0
 for epoch in range(1, args.epochs+1):
-    loss_train, crit_train, reg_train = train(criterion, regularization)
 
-    print('\n====> Train loss: {:.4f}'.format(loss_train))
-    print('\n====> Train criterion: {:.4f}'.format(crit_train))
-    print('\n====> Train regularization: {:.4f}'.format(reg_train))
+    loss_train, crit_train, reg_train = train()
 
     ''' Decrease learning rate if on plateau'''
     scheduler.step(crit_train)
 
-    loss_test, crit_test, reg_test, f1s = test(
-        criterion, regularization, n_runs=5)
+    loss_test, crit_test, reg_test, f1s = test()
 
-    print('\n====> Test loss: {:.4f}'.format(loss_test))
-    print('\n====> Test criterion: {:.4f}'.format(crit_test))
-    print('\n====> Test regularization: {:.4f}'.format(reg_test))
-
+    print('\n====> Train loss: {:.4f}'.format(loss_train))
+    print('====> Train criterion: {:.4f}'.format(crit_train))
+    print('====> Train regularization: {:.4f}'.format(reg_train))
+    print('====> Test loss: {:.4f}'.format(loss_test))
+    print('====> Test criterion: {:.4f}'.format(crit_test))
+    print('====> Test regularization: {:.4f}\n'.format(reg_test))
     print('\n====> Mean F1(t=0.5): {}\n'.format(f1s[9]))
 
-    # Save checkpoint
+    f1 = f1s[9]
+
+    # remember best prec@1 and save checkpoint
+    is_best = f1 > best_f1
+
+    best_f1 = max(f1, best_f1)
     save_checkpoint({
         'epoch': epoch,
         'state_dict': model.state_dict(),
-    }, True, 'models/' + oname + '_epoch_' + str(epoch))
+        'best_f1': best_f1,
+        'optimizer': optimizer.state_dict(),
+    }, is_best, 'models/checkpoint')
+
+    crit_train_list.append(crit_train)
